9b9f45f92a5663507eb5e30b07c0e6df0bf66877
bluejaytodd <jkseo98@hanmail.net>
2019-02-07 00:40:56 +0900

    equi_cc

--- equihashverify.cc
+++ equihashverify.cc
@@ -15,7 +15,7 @@ int verifyEH(const char *hdr, const std::vector<unsigned char> &soln){
 
   // Hash state
   crypto_generichash_blake2b_state state;
-  EhInitialiseState(n, k, state);
+  EhInitialiseState(n, k, state, true ); // cdy_salt true after equihashforkheight
 
   crypto_generichash_blake2b_update(&state, (const unsigned char*)hdr, 140);
 
